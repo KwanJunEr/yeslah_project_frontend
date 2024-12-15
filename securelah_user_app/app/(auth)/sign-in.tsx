1fc8101bf00c944bc6f03ba10f54ee17a3745a66
KwanJunEr <jonastheitguy@gmail.com>
2024-12-16 02:45:50 +0800

    completed partially on sign in and sign up page

--- securelah_user_app/app/(auth)/sign-in.tsx
+++ securelah_user_app/app/(auth)/sign-in.tsx
@@ -1,12 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Image, ScrollView, View, Text, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
+import { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import { Link, router} from "expo-router";
+
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const[isSubmitting, setIsSubmitting] = useState(false)
+  
   return (
-    <View>
-      <Text>SingIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[83vh] px-4 my-6">
+          <Image
+            source={images.appLogo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log In to Aora
+          </Text>
+          <FormField
+            title="Email"
+            // value={form.email}
+            // handleChangeText={(e : any) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
 
-export default SignIn
+          <FormField
+            title="Pasword"
+            // value={form.password}
+            // handleChangeText={(e : any) => setForm({ ...form, password: e })}   
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title = "Sign In"
+            handlePress = {()=>{router.push("/home")}} 
+            containerStyles = "mt-7"
+            isLoading = {isSubmitting}
+          />
+           <View className="justify-center pt-5 flex-row gap-2">
+              <Text className="text-lg text-gray-100 font-pregular">
+                Don't have account?
+              </Text>
+              <Link href ="/sign-up" className="text-lg font-psemibold text-secondary">
+              Sign Up
+              </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  
+  );
+};
+
+export default SignIn;
